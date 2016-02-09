38b442f27fbbbf4b7bab8620b3ce364bb9024d2b
Guillermo RD <gelemias@gmail.com>
2016-02-09 17:21:19 +0100

    finished FrogJmp

--- FrogJmp/FrogJmp/main.m
+++ FrogJmp/FrogJmp/main.m
@@ -9,68 +9,45 @@
 #import <Foundation/Foundation.h>
 
 int solution(int X, int Y, int D);
-int solution1(int X, int Y, int D);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSDate *methodStart = [NSDate date];
-        NSLog(@"--> %d",solution(10, 85, 30));
-        NSLog(@"--> %d",solution(5, 6, 1));
-        NSLog(@"--> %d",solution(0, 1231285, 2));
-        NSLog(@"--> %d",solution(10,1000000000,1));
+        NSLog(@"--> %d",solution(10, 85, 30));                      // 3
+        NSLog(@"--> %d",solution(5, 6, 1));                         // 1
+        NSLog(@"--> %d",solution(0, 1231285, 2));                   // 615643
+        NSLog(@"--> %d",solution(10,1000000000,1));                 // 999999990
+        NSLog(@"--> %d",solution(50,199,4));                        // 38
+        NSLog(@"--> %d",solution(1,5,2));                           // 2
+        NSLog(@"--> %d",solution(5,105,3));                         // 34
+        NSLog(@"--> %d",solution(2,11,3));                          // 3
+        NSLog(@"--> %d",solution(999999999,1000000000,1000000000)); // 1
+        NSLog(@"--> %d",solution(3,999111321,7));                   // 142730189
+        NSLog(@"--> %d",solution(1283,999925368,2));                // 499962043
+        NSLog(@"--> %d",solution(5,1000000000,2));                  // 499999998
         NSDate *methodFinish = [NSDate date];
         NSTimeInterval executionTime = [methodFinish timeIntervalSinceDate:methodStart];
         NSLog(@"executionTime = %fs", executionTime);
-
-        NSDate *m1 = [NSDate date];
-        NSLog(@"--> %d",solution1(10, 85, 30));
-        NSLog(@"--> %d",solution1(5, 6, 1));
-        NSLog(@"--> %d",solution1(0, 1231285, 2));
-        NSLog(@"--> %d",solution1(10,1000000000,1));
-        NSDate *m2 = [NSDate date];
-        NSTimeInterval ex1 = [m2 timeIntervalSinceDate:m1];
-        NSLog(@"executionTime = %fs", ex1);
     }
     return 0;
 }
+
+int solution(int X, int Y, int D) {
+    
+    return ((Y - X)+D-1)/D;
+}
+
 
 // 55%
-int solution(int X, int Y, int D) {
-
-    for (int i=1 ; X < Y ; i++) {
-        X += D;
-        if (X >= Y) {
-            return i;
-        }
-    }
-    
-    return 0;
-}
-
-
-int solution1(int X, int Y, int D) {
-        
-    if (D == 1) {
-        return Y - X;
-    }
-    
-    float raw = roundf((((float)Y-(float)X)/(float)D));
-    return (int)raw;
-}
-
-
-
-/*
- 
- (10,85,30)
- (50,199,4) 38
- (1,5,2)
- (5,105,3) 34
- (2,11,3)
- (999999999,1000000000,1000000000) 1
- (3,999111321,7) 142730189
- (1283,999925368,2) 499962043
- (5,1000000000,2) 499999998
- 
- */
+//int solution(int X, int Y, int D) {
+//
+//    for (int i=1 ; X < Y ; i++) {
+//        X += D;
+//        if (X >= Y) {
+//            return i;
+//        }
+//    }
+//    
+//    return 0;
+//}
